1cc51736d575cd72d9b063bdafa922a0e97af527
yang <hy19835200058@outlook.com>
2023-11-27 18:40:59 +0800

    add feature

--- index.cpp
+++ index.cpp
@@ -3,6 +3,10 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello, World!" << endl;
+    cout << "Hello, World! 你好！ 世界" << endl;
+
+    cout << "Hello, Git!" << endl;
+    cout<<"new feature"<<endl;
+    cout<<"new feature2"<<endl;
     return 0;
 }
